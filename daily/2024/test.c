54d1290cf58f359f6dab453804cacb81517d20ce
zisu17 <s.zisu0417@gmail.com>
2024-02-21 23:56:20 +0900

    C언어 배열과 포인터

---
+++ daily/2024/test.c
@@ -0,0 +1,5 @@
+#include <stdio.h> int main() {
+int code[] = {10, 20, 30 }; int *p = &code;
+printf("%d \n", printf("%d \n", printf("%d \n", printf("%d \n", printf("%d \n", return 0;
+}
+++*p); *p++); *++p); (*p)++); *(p++));
